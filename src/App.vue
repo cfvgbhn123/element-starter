38dc5f08fa46105074adb6549c20e81c94aa6f8e
PC201612061110\Administrator <cfvgbhn123@126.com>
2016-12-14 14:01:24 +0800

    cc

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,42 @@
 <template>
-  <div id="app">
+  <div>
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <el-button @click.native="startHacking">Let's do it</el-button>
+    <hello></hello>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      msg: 'Use Vue 2.0 Today!'
-    }
-  },
+    import hello from './components/hello.vue'
+    export default {
+        data() {
+            return {
+                msg: 'Use Vue 2.0 Today!'
+            }
+        },
 
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It Works',
-        message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
-        duration: 6000
-      })
+        methods: {
+            startHacking() {
+                this.$notify({
+                    title: 'It Works',
+                    message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
+                    duration: 6000
+                })
+            }
+        },
+        components: {
+            hello
+        }
     }
-  }
-}
 </script>
 
-<style>
-body {
-  font-family: Helvetica, sans-serif;
-}
+<style lang="sass">
+    @mixin transform($bc) {
+        transform: $bc
+    }
+    
+    body {
+        font-family: Helvetica, sans-serif;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
 
 Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: (function(h) {
+        return h(App);
+    })
 })
